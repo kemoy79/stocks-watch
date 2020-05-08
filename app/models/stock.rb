67afaa72ce08d2d0734bef8e694dc4208ba8c35b
kemoy79 <kemoy79@gmail.com>
2020-05-08 00:42:30 -0500

    Added iex-client API Key and model class method

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:access_token],
+      secret_token: Rails.application.credentials.iex_client[:secret_key],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+
+    client.price(symbol)
+  end
 end
