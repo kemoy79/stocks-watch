48aee5c4b08b1ea528b77c28799754e2509be587
kemoy79 <kemoy79@gmail.com>
2020-05-09 20:40:08 -0500

    Users can remove stocks from portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,5 +12,18 @@ class UserStocksController < ApplicationController
     flash[:notice] = "#{stock.name} was added to your portfolio"
     redirect_to portfolio_path
   end
+
+  def destroy
+    stock  = Stock.find(params[:id])
+
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+
+    flash[:notice] = "#{stock.name} was successfuly removed form your portfolio!"
+    redirect_to portfolio_path
+
+
+
+  end
   
 end
